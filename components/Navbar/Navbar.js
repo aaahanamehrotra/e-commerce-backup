c6df70b8b9210dc371fcfb005c7160fde86e6eb4
AyonC <e10901ayon@dpsrkp.net>
2022-10-13 17:59:57 +0530

    add: modile responsiveness

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,41 +1,55 @@
+import { useState } from "react";
 import Link from "next/link";
 import { signIn, signOut } from "next-auth/react";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import {
-//   faBagShopping,
-//   faUser,
-//   faMagnifyingGlass as search,
-// } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faMoon,
+  faBars,
+  faXmark
+} from "@fortawesome/free-solid-svg-icons";
 
 import navStyles from "./Navbar.module.css";
 
 function Navbar({ session }) {
+  const [open, setOpen] = useState(false)
+
   return (
     <nav className={navStyles.navbar}>
       <div className={navStyles.title}>
         <Link href="/">E-commerce</Link>
       </div>
-      <div className={navStyles.links}>
+      <div className={[navStyles.links, navStyles.responsive].join(" ")}>
         <Link href="/">Home</Link>
         <Link href="/products/">Products</Link>
         <Link href="/contact">Contact</Link>
+        <Link href="/search">Search</Link>
       </div>
-      <div className={navStyles.links}>
-        <Link href="/search">
-          {/* <FontAwesomeIcon icon={search} className={navStyles.icon} /> */}
-          Search
-        </Link>
+      <div className={[navStyles.links, navStyles.responsive].join(" ")}>
         {/* <Link href="/login"> */}
         {/* <FontAwesomeIcon icon={faUser} className={navStyles.icon} /> */}
         {/* Sign In */}
         {/* </Link> */}
         {session ? (
-          <button onClick={() => signOut()}>Sign out</button>
+          <button onClick={() => signOut()} className={navStyles.sign}>Sign out</button>
         ) : (
-          <button onClick={() => signIn()} type="submit">
+          <button onClick={() => signIn()} type="submit" className={navStyles.sign}>
             Sign in
           </button>
         )}
+      </div>
+      <div className={[navStyles.navbar, navStyles.mobileNav].join(" ")}>
+        {!open ? <FontAwesomeIcon icon={faBars} className={navStyles.hamburger} onClick={() => setOpen(!open)}/> : <FontAwesomeIcon icon={faXmark} className={navStyles.hamburger} onClick={() => setOpen(!open)}/>}
+        {
+        open && 
+        <div className={navStyles.mobileItems}>
+          <div className={[navStyles.links, navStyles.linkItems].join(" ")}>
+            <Link href="/">Home</Link>
+            <Link href="/products/">Products</Link>
+            <Link href="/contact">Contact</Link>
+            <Link href="/search">Search</Link>
+          </div>
+        </div>
+        }
       </div>
     </nav>
   );
